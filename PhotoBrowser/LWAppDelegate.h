5fff54ebda9cf3a9b517f759dd36aa1c6c094086
Lammert Westerhoff <westerhoff@gmail.com>
2013-09-05 13:13:33 +0200

    Initial Commit

---
+++ PhotoBrowser/LWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LWAppDelegate.h
+//  PhotoBrowser
+//
+//  Created by Lammert Westerhoff on 9/5/13.
+//  Copyright (c) 2013 Westerhoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PhotoBrowser/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PhotoBrowser
+//
+//  Created by Lammert Westerhoff on 9/5/13.
+//  Copyright (c) 2013 Westerhoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LWAppDelegate class]));
+    }
+}
